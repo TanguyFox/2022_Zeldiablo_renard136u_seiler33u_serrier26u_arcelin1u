b8f2921733c10bf7da527c6083cae454cba8d8b1
Tanguy Renard <tanguy.renard3@etu.univ-lorraine.fr>
2022-05-25 08:48:06 +0200

    Ajout setters perso

--- zeldiablo/src/gameLaby/laby/Perso.java
+++ zeldiablo/src/gameLaby/laby/Perso.java
@@ -9,7 +9,7 @@ public class Perso {
     /**
      * position du personnage
      */
-    int x, y;
+    private int x, y;
 
     /**
      * constructeur
@@ -35,7 +35,7 @@ public class Perso {
     }
 
     // ############################################
-    // GETTER
+    // GETTER ET SETTER
     // ############################################
 
     /**
@@ -52,5 +52,13 @@ public class Perso {
     public int getY() {
         //getter
         return this.y;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public void setY(int y) {
+        this.y = y;
     }
 }
