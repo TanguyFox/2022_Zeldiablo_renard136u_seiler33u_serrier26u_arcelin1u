622ba00deb3d58b8a15b265e5015a95876e5aca5
ARCELIN Nino <nino.arcelin4@etu.univ-lorraine.fr>
2022-05-31 16:46:41 +0200

    v3

--- zeldiablo/src/gameLaby/laby/LabyDessin.java
+++ zeldiablo/src/gameLaby/laby/LabyDessin.java
@@ -62,10 +62,6 @@ public class LabyDessin implements DessinJeu {
             gc.drawImage(joyaux,amuletteY*30,amuletteX*30,30,30);
         }
 
-<<<<<<< HEAD
-=======
-
->>>>>>> a12e87fa928e950824d3cc2c07cb44ed4ab3e85c
 
         //dessin monstre
         Perso monstre = labyrinthe.monstre;
@@ -86,20 +82,14 @@ public class LabyDessin implements DessinJeu {
         // dessin Perso
         gc.setFill(Color.RED);
         Perso perso = labyrinthe.pj;
-<<<<<<< HEAD
+
         FileInputStream inputStream3 = new FileInputStream("zeldiablo/images/personnage2.png");
-=======
-        FileInputStream inputStream3 = new FileInputStream("zeldiablo/images/personnage.jpg");
->>>>>>> a12e87fa928e950824d3cc2c07cb44ed4ab3e85c
+
         Image personnage = new Image(inputStream3);
         int px = perso.getX();
         int py = perso.getY();
         gc.drawImage(personnage,py*30,px*30,30,30);
-<<<<<<< HEAD
 
-
-=======
->>>>>>> a12e87fa928e950824d3cc2c07cb44ed4ab3e85c
     }
 
 }
